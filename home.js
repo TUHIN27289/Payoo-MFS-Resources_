6cdd8486f26f02dda270aba0937f17849301a982
Toukir <toukerahmed.2728@gmail.com>
2025-08-27 19:51:07 +0600

    27-5 Use Shared Function to Get Inner Text and Set Inner Text

--- home.js
+++ home.js
@@ -1,22 +1,117 @@
 // console.log('home js connected')
 
 // add money functionality
+
+// const validPin = 1234;
+// document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   const bankInfo = document.getElementById("bank").value;
+//   const accountNumber = document.getElementById("account-number").value;
+//   const addAmount = parseInt(document.getElementById("amount-add").value);
+//   const uPin = parseInt(document.getElementById("u-Pin").value);
+//   const availableBalance = parseInt(
+//     document.getElementById("available-balance").innerText
+//   );
+//   if (accountNumber.length < 11 || accountNumber.length > 11) {
+//     alert("Account number must be 11 digit");
+//     return;
+//   }
+
+//   if (uPin.length < 4 || uPin.length > 4) {
+//     alert("Pin must be 4 digit");
+//     return;
+//   } else if (uPin !== validPin) {
+//     alert("Invalid pin");
+//     return;
+//   }
+
+// //   const totalBalance = availableBalance + addAmount;
+// //   console.log(
+// //     bankInfo,
+// //     accountNumber,
+// //     addAmount,
+// //     uPin,
+// //     availableBalance,
+// //     totalBalance
+// //   );
+//   document.getElementById("available-balance").innerText = totalBalance;
+// });
+
+// //  cash out functionality
+// document.getElementById("cashOutBtn").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   const acctNumber = document.getElementById("acc-number").value;
+//   const cashOutAmount = parseInt(
+//     document.getElementById("withdraw-amount").value);
+//   const pin = parseInt(document.getElementById("cash-out-Pin").value);
+// //   console.log(acctNumber, cashOutAmount, pin);
+//   const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+//   console.log(availableBalance);
+//   if (acctNumber.length < 11 || acctNumber.length > 11) {
+//     alert("Account number must be 11 digit");
+//     return;
+//   }
+//   if(pin.length<4|| pin.length>4){
+//     alert('Pin must be 4 digit')
+//     return;
+//   }
+//   else if(pin!==validPin){
+//     alert('Invalid pin')
+//     return;
+//   }
+//   const totalBalance = availableBalance - cashOutAmount;
+//   document.getElementById("available-balance").innerText = totalBalance;
+// });
+
+
+
+
+
+
+
+// ~~~.      /.....................\~ ~ ~
+// updated functionality and better and using function
+// add money button
+function takeInt(id) {
+  const data = document.getElementById(id);
+  const acData = data.value;
+  const conData = parseInt(acData);
+  return conData;
+}
+function takeString(id) {
+  const data = document.getElementById(id);
+  const acData = data.value;
+  const conData = acData;
+  return conData;
+}
+function innerValue(id){
+  const inVal =document.getElementById(id).innerText;
+  const conVal=parseInt(inVal);
+  return conVal;
+}
+function setValue(id,val){
+  document.getElementById(id).innerText=val
+}
+
+const validPhn = 12345678912;
 const validPin = 1234;
+
 document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
   e.preventDefault();
-  const bankInfo = document.getElementById("bank").value;
-  const accountNumber = document.getElementById("account-number").value;
-  const addAmount = parseInt(document.getElementById("amount-add").value);
-  const uPin = parseInt(document.getElementById("u-Pin").value);
-  const availableBalance = parseInt(
-    document.getElementById("available-balance").innerText
-  );
-  if (accountNumber.length < 11 || accountNumber.length > 11) {
+  const bankInfo = takeString("bank");
+  const accountNumber = takeString("account-number");
+  const addAmount = takeInt("amount-add");
+  const uPin = takeInt("u-Pin");
+  // const availableBalance=takeInt('available-balance');
+  // const availableBalance = parseInt(
+  //   document.getElementById("available-balance").innerText
+  // );
+  const availableBalance=innerValue('available-balance');
+  if (accountNumber.length > 11 || accountNumber.length < 11) {
     alert("Account number must be 11 digit");
     return;
   }
-
-  if (uPin.length < 4 || uPin.length > 4) {
+  if (uPin.length > 4 || uPin.length < 4) {
     alert("Pin must be 4 digit");
     return;
   } else if (uPin !== validPin) {
@@ -24,42 +119,33 @@ document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
     return;
   }
   const totalBalance = availableBalance + addAmount;
-  console.log(
-    bankInfo,
-    accountNumber,
-    addAmount,
-    uPin,
-    availableBalance,
-    totalBalance
-  );
-  document.getElementById("available-balance").innerText = totalBalance;
+  // document.getElementById("available-balance").innerText = totalBalance;
+  setValue('available-balance',totalBalance);
 });
 
-//  cash out functionality
-document.getElementById("cashOutBtn").addEventListener("click", function (e) {
+// cash out button
+document.getElementById('cashOutBtn').addEventListener('click',function(e){
   e.preventDefault();
-  const acctNumber = document.getElementById("acc-number").value;
-  const cashOutAmount = parseInt(
-    document.getElementById("withdraw-amount").value);
-  const pin = parseInt(document.getElementById("cash-out-Pin").value);
-  console.log(acctNumber, cashOutAmount, pin);
-  const availableBalance = parseInt(document.getElementById("available-balance").innerText);
-  console.log(availableBalance);
-  if (acctNumber.length < 11 || acctNumber.length > 11) {
-    alert("Account number must be 11 digit");
+  const acctNumber=takeString('acc-number');
+  const withdrawAmount=takeInt('withdraw-amount');
+  const cashOutPin=takeInt('cash-out-Pin');
+  // const availableBalance=document.getElementById('available-balance').innerText;
+  const availableBalance=innerValue('available-balance');
+  if(acctNumber.length<11||acctNumber.length>11){
+    alert('Account number must be 11 digit')
     return;
   }
-  if(pin.length<4|| pin.length>4){
+  if(cashOutPin.length>4||cashOutPin.length<4){
     alert('Pin must be 4 digit')
     return;
-  }
-  else if(pin!==validPin){
+  }else if(cashOutPin!==validPin){
     alert('Invalid pin')
     return;
   }
-  const totalBalance = availableBalance - cashOutAmount;
-  document.getElementById("available-balance").innerText = totalBalance;
-});
+  const totalBalance=availableBalance-withdrawAmount;
+  // document.getElementById('available-balance').innerText=totalBalance;
+  setValue('available-balance', totalBalance);
+})
 
 // toggle function
 // display add money
--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 //     const pin=1234;
 //     const uNum= document.getElementById('pNum').value
 //     const nConverted=parseInt(uNum);
-    
+
 //     const uPin=document.getElementById('uPin').value
 //     const convertedPin=parseInt(uPin)
 //         // console.log(nConverted,convertedPin)
@@ -17,22 +17,43 @@
 //         alert('Invalid credentials')
 //     }
 //     }
-        
-// )
 
+// )
 
 // updated login functionality and better
 
-document.getElementById('loginBtn').addEventListener('click',function(e){
-    e.preventDefault();
-    const validPhn=12345678912;
-    const validPin=1234;
-    const userPhn=parseInt(document.getElementById('pNum').value);
-    const userPin=parseInt(document.getElementById('uPin').value);
-    if(validPhn===userPhn&&validPin===userPin){
-        window.location.href='./home.html'
-    }
-    else{
-        alert('Invalid credentials')
-    }
-})
+// document.getElementById('loginBtn').addEventListener('click',function(e){
+//     e.preventDefault();
+//     const validPhn=12345678912;
+//     const validPin=1234;
+//     const userPhn=parseInt(document.getElementById('pNum').value);
+//     const userPin=parseInt(document.getElementById('uPin').value);
+//     if(validPhn===userPhn&&validPin===userPin){
+//         window.location.href='./home.html'
+//     }
+//     else{
+//         alert('Invalid credentials')
+//     }
+// })
+
+// updated login functionality and better and using function
+
+function takeInt(id) {
+  const data = document.getElementById(id);
+  const acData = data.value;
+  const conData = parseInt(acData);
+  return conData;
+}
+const validPhn = 12345678912;
+const validPin = 1234;
+document.getElementById("loginBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const userPhn = takeInt("pNum");
+  const userPin = takeInt("uPin");
+  console.log(userPhn, userPin);
+  if ((validPhn === userPhn) && (validPin === userPin)) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid credentials");
+  }
+});
